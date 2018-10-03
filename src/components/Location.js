f69e9520737bbfd3a5ca51df59e7f9f57f013277
Juan Fernando Restrepo <JuanRestrepo>
2018-10-02 20:46:10 -0500

    Se identa código

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Location = ({ city }) => {
-  //Destructuring => Cuando tenenmos una propiedad que luego queremos asignar a una variable o constante
-  //con el mismo nombre de la propiedad utilizamos esa técnica
+//Destructuring => Cuando tenenmos una propiedad que luego queremos asignar a una variable o constante
+//con el mismo nombre de la propiedad utilizamos esa técnica
 
-  return (
-      <div><h1>{ city }</h1></div>
-  );
+const Location = ({ city }) => (
 
-};
+      <div>
+        <h1>
+          { city }
+        </h1>
+      </div>
+
+);
 
 export default Location;
