1b0a0c530b8e8db8e9921b60362b7775195ba3a0
Juan Fernando Restrepo <JuanRestrepo>
2018-10-26 15:02:56 -0500

    Se le agregan stilos css para iconos del clima

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -4,8 +4,11 @@ import WeatherTemperature from './WeatherTemperature';
 
 const WeatherData = () => (
   <div>
-    <WeatherTemperature />
-    <WeatherExtraInfo />
+    <WeatherTemperature
+      temperature={20}
+      weatherState={''}
+    />
+    <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
   </div>
 );
 
--- src/components/WeatherExtraInfo.js
+++ src/components/WeatherExtraInfo.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const WeatherExtraInfo = () => (
-  <div>WeatherExtraInfo</div>
+const WeatherExtraInfo = ({ humidity, wind }) => (
+
+  <div>
+    <span>{`${humidity} % - `}</span>
+    <span>{`${wind} wind`}</span>
+  </div>
+
 );
 
 export default WeatherExtraInfo;
+/**
+ el template string se comporta como los string de scala (ecma script 6, el ejemplo está en los span anteriores)
+**/
